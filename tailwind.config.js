22f0dc1a6df33dbd348affc34ff48872aacc3d1b
Simon Agbey <simon@simon.simon>
2023-05-03 21:56:31 +0100

    update of UI

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./ui/**/*.{js,jsx,ts,tsx}', './client/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- ui/pages/Home.jsx
+++ ui/pages/Home.jsx
@@ -1,108 +1,114 @@
-/* eslint-disable import/no-unresolved */
-// This is the code for the home page
 import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { usePeopleData } from '../hooks/usePeopleData';
 import { useCommunitiesData } from '../hooks/useCommunitiesData';
-import { EventSelector } from './selector/EventSelector';
+import { EventSelector } from './EventSelector';
 import { Texts } from '../../infra/constants';
-// This is the code for the date format
+
+// Format the given date string
 const formatDate = (date) => {
   if (!date) return 'N/A';
   const d = new Date(date);
   return `${d.toLocaleDateString()} ${d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
 };
+
+// Utility function to join class names
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+// Home component
 export const Home = () => {
-  // This is the code for the event selector
- const [selectedEvent, setSelectedEvent] = useState('');
- // This is the code for the list of events
+  // State for the selected event
+  const [selectedEvent, setSelectedEvent] = useState('');
+  // Fetch communities data
   const communities = useCommunitiesData();
+  // Fetch people data
   const people = usePeopleData();
-// This is the code for the list of people registered in the selected event
+
+  // Filter people based on the selected event
   const filteredPeople = selectedEvent
     ? people.filter((person) => person.communityId === selectedEvent)
     : [];
-    // This is the code for the check-in button
+
+  // Function to handle check-in
   const handleCheckIn = (person) => {
     Meteor.call('people.checkIn', person._id);
   };
-// This is the code for the check-out button
+
+  // Function to handle check-out
   const handleCheckOut = (person) => {
     Meteor.call('people.checkOut', person._id);
   };
- const peopleInEventNow = filteredPeople.filter(person => person.checkInDate && !person.checkOutDate).length;
-// This is the code for the people by company in the event now
+
+  // Calculate the number of people in the event right now
+  const peopleInEventNow = filteredPeople.filter(person => person.checkInDate && !person.checkOutDate).length;
+
+  // Calculate the number of people by company in the event right now
   const peopleByCompanyInEventNow = filteredPeople.reduce((acc, person) => {
     if (person.checkInDate && !person.checkOutDate) {
-      acc[person.company] = (acc[person.company] || 0) + 1;
+      acc[person.companyName] = (acc[person.companyName] || 0) + 1;
     }
     return acc;
   }, {});
-// This is the code for the people not checked-in
+
+  // Calculate the number of people not checked-in
   const peopleNotCheckedIn = filteredPeople.filter(person => !person.checkInDate).length;
-// This is the code for the summary of the event
+
+  // Generate company stats string
   const companyStats = Object.entries(peopleByCompanyInEventNow)
     .map(([company, count]) => `${company} (${count})`)
     .join(', ');
 
+  // Render the Home component
   return (
-    <div>
-      {/* This is the code for the event selector */}
-      <div className="EventSelector">
-   <h1>{Texts.HOME_TITLE}</h1>
-      <EventSelector events={communities} onChange={(e) => setSelectedEvent(e.target.value)} />
-      </div>
-      {/*  This is the code for the summary of the event */}
- <div className="summary">
-<h3>Summary</h3>
-      <p>People in the event right now: {peopleInEventNow}</p>
-      <p>People by company in the event right now: {companyStats}</p>
-      <p>People not checked-in: {peopleNotCheckedIn}</p>
- </div>
- {/*  This is the code for the list of people registered in the selected event */}
-    <div className="container">
-      <h2>People Registered in the Selected Event</h2>
-      <ul className="ul">
-        {filteredPeople.map((person) => (
-          <li className="li" key={person._id}>
-            {person.firstName} {person.lastName}, {person.companyName}, {person.title}
-          </li>
-        ))}
-      </ul>
-    </div>
-    {/* This is the code for the check-in and check-out buttons */}
- <div className="container">
-      <h2>Registered People</h2>
-      <ul className="ul">
-        {filteredPeople.map((person) => {
-          const fullName = `${person.firstName} ${person.lastName}`;
-          const checkedIn = person.checkInDate && Date.now() - new Date(person.checkInDate) > 5000;
+    // Rest of the JSX code
+  <>
+    <div className="relative isolate overflow-hidden bg-gray-100 py-12 sm:py-24">
+      <div className="container mx-auto px-4">
+        <header className="mb-12">
+          <h2 className="text-4xl font-bold tracking-tight text-gray-500 sm:text-4xl mb-4">{Texts.HOME_TITLE}</h2>
+          <EventSelector events={communities} onChange={(e) => setSelectedEvent(e.target.value)} />
+        </header>
+        <main className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <section className="border border-gray-300 p-4">
+            <h3 className="text-xl text-gray-600 font-bold">Summary</h3>
+            <p className="mt-6 text-lg leading-8 text-gray-500">People in the event right now: <span className='text-lg p-2 text-indigo-600'>{peopleInEventNow}</span></p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">People by company in the event right now:<span className='text-lg p-2 text-indigo-600'>{companyStats}</span></p>
+            <p className="mt-6 text-lg leading-8 text-gray-700">People not checked-in: <span className='text-lg p-2 text-indigo-600'>{peopleNotCheckedIn}</span></p>
+          </section>
+          <section className="col-span-2">
+            <h2 className="text-2xl font-semibold tracking-tight mb-4">People Registered in the Selected Event</h2>
+            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {filteredPeople.map((person) => {
+                const fullName = `${person.firstName} ${person.lastName}`;
+                const checkedIn = person.checkInDate && Date.now() - new Date(person.checkInDate) > 5000;
 
-          return (
-            <li className="li" key={person._id}>
-              <div>
-                {fullName}, {person.companyName}, {person.title}
-              </div>
-              <div>
-                Check-in: {formatDate(person.checkInDate)}
-              </div>
-              <div>
-                Check-out: {formatDate(person.checkOutDate)}
-              </div>
-              {checkedIn ? (
-                <button className="button" onClick={() => handleCheckOut(person)}>
-                  Check-out {fullName}
-                </button>
-              ) : (
-                <button className="button" onClick={() => handleCheckIn(person)}>
-                  Check-in {fullName}
-                </button>
-              )}
-            </li>
-          );
-        })}
-      </ul>
- </div>
+                return (
+                  <li className="border border-gray-300 p-4" key={person._id}>
+                    <div>
+                      {fullName}, {person.companyName}, {person.title}
+                    </div>
+                    <div>
+                      <span>Check-in:</span>{formatDate(person.checkInDate)}
+                    </div>
+                    <div>
+                      <span>Check-out:</span> {formatDate(person.checkOutDate)}
+                    </div>
+                    <button
+                      className={`mt-2 rounded-md px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${checkedIn ? 'bg-red-600 hover:bg-red-500 focus-visible:outline-red-600' : 'bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600'}`}
+                      onClick={() => (checkedIn ? handleCheckOut(person) : handleCheckIn(person))}
+                    >
+                      <span>{checkedIn ? 'Check-out:' : 'Check-in:'}</span> {fullName}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </main>
+      </div>
     </div>
-  );
-};
+  </>
+);
+            }
